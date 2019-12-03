fec0a7321e0206ab847a493e2439deaea6240959
Cameron <gabrilend@gmail.com>
2019-12-03 01:12:51 -0500

    Part 2 of the tutorial completed

--- input_handlers.py
+++ input_handlers.py
@@ -3,7 +3,7 @@ import tcod as libtcod
 def handle_keys(key):
     # Movement keys
     if key.vk == libtcod.KEY_UP:
-        return {'move': (0, -2)}
+        return {'move': (0, -1)}
     elif key.vk == libtcod.KEY_DOWN:
         return {'move': (0, 1)}
     elif key.vk == libtcod.KEY_LEFT:
