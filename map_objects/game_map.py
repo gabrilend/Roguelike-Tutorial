0913260975086b4e7bb45498a671cf2494f5687b
Cameron <gabrilend@gmail.com>
2019-12-05 16:11:19 -0500

    Part 6 completed wowee
    combat!

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -4,6 +4,10 @@ import tcod as libtcod
 from entity import Entity
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from components.ai import BasicMonster
+from components.fighter import Fighter
+from render_functions import RenderOrder
+
 
 
 class GameMap:
@@ -108,8 +112,12 @@ class GameMap:
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                    fighter_component = Fighter(hp=20, defense=0, power=3)
+                    ai_component = BasicMonster()
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 else:
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 
                 entities.append(monster)
